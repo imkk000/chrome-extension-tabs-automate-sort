f1902597034f2462b213b323b334c96281adb48c
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2019-01-06 20:41:53 +0700

    [Added]: chrome storage for store active config

--- js/background.js
+++ js/background.js
@@ -1,7 +1,15 @@
 'use strict'
 
 // global variable
-let active = false
+let active
+
+function setAppStatus() {
+  chrome.storage.sync.set({ appStatus: active }, null)
+}
+
+function getAppStatus() {
+  chrome.storage.sync.get('appStatus', result => active = result.appStatus)
+}
 
 function extractHostname(url) {
   let hostname
@@ -20,9 +28,8 @@ function extractRootDomain(url) {
     arrLen = splitArr.length
   if (arrLen > 2) {
     domain = splitArr[arrLen - 2] + '.' + splitArr[arrLen - 1]
-    if (splitArr[arrLen - 2].length == 2 && splitArr[arrLen - 1].length == 2) {
+    if (splitArr[arrLen - 2].length == 2 && splitArr[arrLen - 1].length == 2)
       domain = splitArr[arrLen - 3] + '.' + domain
-    }
   }
   return domain
 }
@@ -32,14 +39,11 @@ function compare(a, b) {
 }
 
 function sort(tabs) {
-  if (!!!tabs) return []
   return tabs.sort((a, b) => {
-    const ad = extractRootDomain(a.favIconUrl)
-    const bd = extractRootDomain(b.favIconUrl)
-    if (ad != bd)
-      return compare(ad, bd)
-    if (a.title != b.title)
-      return compare(a.title, b.title)
+    const axrd = extractRootDomain(a.favIconUrl)
+    const bxrd = extractRootDomain(b.favIconUrl)
+    if (axrd != bxrd) return compare(axrd, bxrd)
+    if (a.title != b.title) return compare(a.title, b.title)
     return compare(a.url, b.url)
   })
 }
@@ -47,30 +51,30 @@ function sort(tabs) {
 function sortTabsInCurrentWindow() {
   if (!active) return
   chrome.tabs.query({}, tabs => {
-    const tabsPinned = sort(tabs.filter(tab => tab.pinned))
-    const tabsUnpinned = sort(tabs.filter(tab => !tab.pinned))
-    const newTabs = [].concat(tabsPinned, tabsUnpinned)
-    newTabs.forEach((tab, index) => {
-      chrome.tabs.move(tab.id, { index }, null)
-    })
+    [].concat(
+      sort(tabs.filter(tab => tab.pinned)),
+      sort(tabs.filter(tab => !tab.pinned)))
+      .forEach((tab, index) => {
+        chrome.tabs.move(tab.id, { index }, null)
+      })
   })
 }
 
 function setIcon() {
   const activeIconFile = active ? 'active' : 'inactive'
   chrome.browserAction.setIcon({ path: 'icon/' + activeIconFile + '.png' })
-  console.log('app', active ? "on" : "off")
+  console.log('app:', active ? "on" : "off")
 }
 
-function appIconOnClick(tab) {
+function appIconOnClick() {
   active = !active
+  setAppStatus()
   setIcon()
-  if (active)
-    sortTabsInCurrentWindow()
+  if (active) sortTabsInCurrentWindow()
 }
 
 // onload app
 setIcon()
 chrome.browserAction.onClicked.addListener(appIconOnClick)
-chrome.tabs.onRemoved.addListener(() => sortTabsInCurrentWindow())
-chrome.tabs.onUpdated.addListener(() => sortTabsInCurrentWindow())
+chrome.tabs.onRemoved.addListener(sortTabsInCurrentWindow)
+chrome.tabs.onUpdated.addListener(sortTabsInCurrentWindow)
